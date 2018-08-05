db822166862f7d318c7ba90d01c0162028a0877b
jzhw0130 <569274132@qq.com>
2018-08-05 21:16:26 +0800

    Init

---
+++ AutoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoDemo
+//
+//  Created by jingzhiwei on 1/8/18.
+//  Copyright © 2018 jingzhiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoDemo
+//
+//  Created by jingzhiwei on 1/8/18.
+//  Copyright © 2018 jingzhiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
